2d2ff799265e6b7baac3ed49e40391895948bf06
eralp <eralpsengoz@gmail.com>
2023-01-18 12:09:55 +0600

    removing outdated files

--- commands/mylo.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('mylo')
-		.setDescription('This is my honest reaction to that information'),
-	async execute(interaction) {
-		await interaction.reply("https://cdn.discordapp.com/attachments/898894412724187176/1064057188940795934/IMG_3122.png");
-	},
-};
--- commands/pong.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('pong')
-		.setDescription('Replies with Ping!'),
-	async execute(interaction) {
-		await interaction.reply(`Ping!`);
-	},
-};
